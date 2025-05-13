cb15b9db6cebbf71510df4e09c4603ef141c7db0
FatimaZelay <fatima.elay@gmail.com>
2025-05-13 18:48:50 +0200

    feat: added routes in server.js #9
    added routes of index & the playlist.liquid

--- server.js
+++ server.js
@@ -5,34 +5,68 @@ import express from 'express'
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
+
+console.log('')
+// Doe een fetch naar de data die je nodig hebt
+// const apiResponse = await fetch('...')
+
+// Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
+// const apiResponseJSON = await apiResponse.json()
+
+// Controleer eventueel de data in je console
+// (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
+// console.log(apiResponseJSON)
+
+
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
-
-// Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
 // Stel Liquid in als 'view engine'
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
-
+// Maak een GET route voor de index (meestal doe je dit in de root, als /)
 app.get('/', async function (request, response) {
-  response.render('index.liquid')
+  const apiResponse = await fetch ('https://fdnd-agency.directus.app/items/tm_playlist');
+
+  const apiResponseJSON = await apiResponse.json ();
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('index.liquid', { playlists: apiResponseJSON.data});
+});
+
+
+
+app.post('/', async function (request, response) {
+
+  response.redirect(303, '/')
 })
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8000)
+app.get('/playlist', async function (request, response)  {
 
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+  const likedapiResponse = await fetch('https://fdnd-agency.directus.app/items/tm_playlist')
+  const likedResponseJSON  = await likedapiResponse.json() 
+
+  response.render('playlist.liquid', { playlists: likedResponseJSON.data, succes_message: request.query.succes });
+});
+
+
+
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8001)
+
+// Start Express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  console.log(`Project draait via http://localhost:${app.get('port')}/\n\nSucces deze sprint. En maak mooie dingen! 🙂`)
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
 })
