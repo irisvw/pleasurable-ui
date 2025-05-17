d02c835da3e920ca173d74bb5ebca89659550228
FatimaZelay <fatima.elay@gmail.com>
2025-05-17 09:12:16 +0200

    fix: poort terug naar 8000 #9

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get('/playlist', async function (request, response)  {
   response.render('playlist.liquid', { playlists: playlistResponseJSON.data });
 });
 
+app.set('port', process.env.PORT || 8000)
 
 
 
@@ -64,6 +65,5 @@ app.set('port', process.env.PORT || 8001)
 
 // Start Express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
+  console.log(`Project draait via http://localhost:${app.get('port')}/\n\nSucces deze sprint. En maak mooie dingen! 🙂`)
 })
